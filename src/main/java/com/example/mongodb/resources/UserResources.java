e52be88bba94c34d74ccb72e2c32d50df6f558a8
danidbd <dbrumcassimiro@gmail.com>
2023-11-19 19:53:17 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/example/mongodb/resources/UserResources.java
@@ -0,0 +1,25 @@
+package com.example.mongodb.resources;
+
+import com.example.mongodb.domain.User;
+import com.example.mongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResources {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
