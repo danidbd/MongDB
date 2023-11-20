3e2c671a022108785a2ca938a6a867c9b63db84e
danidbd <dbrumcassimiro@gmail.com>
2023-11-20 09:48:31 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/example/mongodb/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.mongodb.resources.exception;
+
+import com.example.mongodb.domain.User;
+import com.example.mongodb.dto.UserDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
+}
--- src/main/java/com/example/mongodb/services/UserService.java
+++ src/main/java/com/example/mongodb/services/UserService.java
@@ -3,10 +3,13 @@ package com.example.mongodb.services;
 import com.example.mongodb.domain.User;
 import com.example.mongodb.repository.UserRepository;
 
+import com.example.mongodb.resources.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     @Autowired
@@ -14,6 +17,10 @@ public class UserService {
 
     public List<User> findAll (){
         return repo.findAll();
+    }
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
